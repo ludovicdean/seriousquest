7bbc20a829552c8160c55913c755cb3fb35b79c5
ludovicdean <ludovicd58@gmail.com>
2024-10-23 23:43:24 +0200

    modifications suite à rdv 15/10

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ludovic.dean.github.io',
   base: '/seriousquest',
-  integrations: [tailwind()]
+  integrations: [tailwind(), icon()]
 });
