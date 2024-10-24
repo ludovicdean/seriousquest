c5af389ece64e60c51b87791cc7d8975ee9ce439
ludovicdean <ludovicd58@gmail.com>
2024-10-25 00:36:36 +0200

    modifications suite à rdv 15/10

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,14 @@
+const {addIconSelectors} = require('@iconify/tailwind');
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				'accent-orange': "var(--accent-orange)",
-				'accent-yellow': "var(--accent-yellow)",
-			}
-		},
-	},
-	plugins: [],
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+        extend: {
+            colors: {
+                'accent-orange': "var(--accent-orange)",
+                'accent-yellow': "var(--accent-yellow)",
+            }
+        },
+    },
+    plugins: [],
 }
